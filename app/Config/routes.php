ddc80343048c69950440057b6cbc0290059504cb
Andrew LeCody <andrewlecody@gmail.com>
2012-12-22 17:13:25 -0600

    Minor route change / now goes to Categories->index

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -25,7 +25,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'categories', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
